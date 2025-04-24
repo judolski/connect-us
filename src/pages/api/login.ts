9933fda28005447d831dfa1c4f9df17bc4eef863
Chidi J.  Owo <33990325+judolski@users.noreply.github.com>
2025-04-24 23:24:13 +0100

    recent update 3

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -7,6 +7,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseModel>
 ) {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      success: false,
+      statusCode: 405,
+      message: "Method Not Allowed",
+      data: null,
+    });
+  }
+
   await connectToDatabase();
 
   const { username, password } = req.body;
@@ -21,15 +30,15 @@ export default async function handler(
   const token = generateToken(authUser.data);
 
   if (!token) {
-    return res.status(authUser.statusCode).json({
-      success: true,
+    return res.status(400).json({
+      success: false,
       statusCode: 400,
-      message: "An error occured while logging in",
+      message: "An error occurred while logging in",
       data: null,
     });
   }
 
-  return res.status(authUser.statusCode).json({
+  return res.status(200).json({
     success: true,
     statusCode: 200,
     message: "Success",
