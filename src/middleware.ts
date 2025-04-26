c248e9b35baa60c0596037235be6618d6e0593b3
Chidi J.  Owo <33990325+judolski@users.noreply.github.com>
2025-04-26 17:08:32 +0100

    integrated pusher for realtime communication 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,22 @@
 // middleware.ts
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { verifyToken } from "./utils/auth";
 
-export async function middleware(request: Request) {
+export async function middleware(request: NextRequest) {
   const token = request.headers.get("authorization");
+
   if (!token) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
+
   const response = await verifyToken(token);
 
   if (response?.success) {
     const res = NextResponse.next();
-    // res.headers.set("x-user", JSON.stringify(response.data));
+
     res.cookies.set("user-info", JSON.stringify(response.data), {
       httpOnly: true,
-      path: "/", // send to all routes
+      path: "/",
       secure: process.env.NODE_ENV === "production",
       sameSite: "strict",
     });
@@ -27,5 +28,5 @@ export async function middleware(request: Request) {
 }
 
 export const config = {
-  matcher: ["/api/messages/:path*", "/api/users/:path*"], // Add routes you want to protect
+  matcher: ["/api/messages/:path*", "/api/users/:path*"],
 };
